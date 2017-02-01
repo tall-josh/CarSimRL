more bug fixes. Updating the wrong value to q_target

Obstacle_Avoidance_Q_Learning/Obstacle_Avoidance_Q_Learning/deep_q_neural_network.py
@@ -65,7 +65,7 @@ def new_conv_layer(prev_layer,
         # It calculates max(x, 0) for each input pixel x.
         # This adds some non-linearity to the formula and allows us
         # to learn more complicated functions.
-        layer = tf.nn.relu(layer)
+        layer = tf.nn.sigmoid(layer)
 
     # Note that ReLU is normally executed before the pooling,
     # but since relu(max_pool(x)) == max_pool(relu(x)) we can
@@ -121,7 +121,7 @@ def new_fc_layer(prev_layer,          # The previous layer.
             
             # Use ReLU?
             if use_relu:
-                layer = tf.nn.relu(layer)
+                layer = tf.nn.sigmoid(layer)
             
             tf.summary.histogram(layer_name+"_output", layer)
             
@@ -243,7 +243,8 @@ def fitBatch(batch_state, batch_target, save=False, verbose=False, iteration_cou
     state_action_dict = {state_mat: state_feed, q_target: q_feed}  
     
     if verbose:
-        session.run(optimizer, feed_dict = state_action_dict)
+        loss = session.run([cost, optimizer], feed_dict = state_action_dict)
+        print("Loss: {0}".format(loss))
         result = session.run(merged_summ, feed_dict = state_action_dict)
         writer.add_summary(result, iteration_count)
     else: 

Obstacle_Avoidance_Q_Learning/Obstacle_Avoidance_Q_Learning/main.py
@@ -159,12 +159,12 @@ def initSimulation(car, state, filling_buffer = False):
 
     ########## I'M GOING WHERE THE ACTION ISSSS!!!!! ################
 total_frames = 0
-epochs = 100
+epochs = 1000
 epoch_cnt = 0
 gamma = 0.9
 epsilon = 1
 leave_program = False
-batch_size = 2
+batch_size = 5
 buffer = 50
 replay = []
 h = 0
@@ -246,18 +246,17 @@ for i in range(epochs):
             batch = random.sample(replay, batch_size)
             target_batch = []
             for element in batch:
-                old_state, action, reward, new_state = element
-                q_mat_old = dqnn.getQMat(old_state)
+                replay_old_state, replay_action_idx, replay_reward, replay_new_state = element
+                q_mat_old = dqnn.getQMat(replay_old_state)
 
                 y =  copy.deepcopy(q_mat_old[0])
 
-                q_mat_new = dqnn.getQMat(new_state)
-                q_val_new = np.argmax(q_mat_new)
-
-                target_q = reward + (gamma*q_val_new)
-                y[action] = target_q
-#                if total_frames % 100 == 0:
-#                    print("Y: {0}, idx: {1}, target: {2}, epsilon: {3}".format(y, action, target_q, epsilon))
+                q_mat_new = dqnn.getQMat(replay_new_state)[0]
+                q_val_new = max(q_mat_new)
+                q_update = replay_reward + (gamma*q_val_new)
+                y[replay_action_idx] = q_update
+                if total_frames % 10 == 0:
+                    print("Y: {0}, idx: {1}, target: {2} (ori: {3}), epsilon: {4}".format(y, replay_action_idx, q_update, q_val_new, epsilon))
                     
                 target_batch.append(y)
 